d0bcb52a8baed0f7c42795ace982136863d2ebb8
Lance Lakey <lancelakey@gmail.com>
2012-01-11 02:34:04 -0800

    added methodobject comments to ex41_0.rb

--- ex41_0.rb
+++ ex41_0.rb
@@ -13,6 +13,14 @@ def death()
   # Then in bash if you echo $? you'll see this script did exit with error code 1
 end
 
+# ROOMS is a hash containing keys and values
+# ROOMS only contains one key,value
+# The key is the symbol :death
+# The value is a methodobject :death
+#
+# There's a method on ruby called "death"
+# I'm asking ruby to give me "death" as a methodobject
+# I'm taking that methodobject and storing it as a value in a hash called ROOMS using the symbol :death as the key
 ROOMS = {
   :death => method(:death),
 }
